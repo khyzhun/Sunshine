596151b6a71313b4628e8337dc0eb08328343708
khyzhun <sasha.khyzhun@gmail.com>
2024-10-26 23:37:11 +0300

    init

---
+++ shared/src/androidMain/kotlin/com/khyzhun/sunshine/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.khyzhun.sunshine
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
