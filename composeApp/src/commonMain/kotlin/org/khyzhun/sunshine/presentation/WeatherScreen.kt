bc71587be6b2f0e9f8ba2fe9bad1ac03f909dbeb
khyzhun <sasha.khyzhun@gmail.com>
2024-10-31 00:28:02 +0200

    Resources: colors, strings;

--- composeApp/src/commonMain/kotlin/org/khyzhun/sunshine/presentation/WeatherScreen.kt
+++ composeApp/src/commonMain/kotlin/org/khyzhun/sunshine/presentation/WeatherScreen.kt
@@ -1,38 +1,37 @@
 package org.khyzhun.sunshine.presentation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.resources.stringResource
+import org.khyzhun.sunshine.theme.AppColors
 import sunshine.composeapp.generated.resources.Res
-import sunshine.composeapp.generated.resources.allDrawableResources
-import sunshine.composeapp.generated.resources.compose_multiplatform
 import sunshine.composeapp.generated.resources.ic_sunny
+import sunshine.composeapp.generated.resources.sunny
 
 @Composable
 fun WeatherScreen(viewModel: WeatherViewModel) {
@@ -41,7 +40,7 @@ fun WeatherScreen(viewModel: WeatherViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF4FC3F7)) // Light blue background
+            .background(AppColors.SunnyBackground)
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -61,7 +60,7 @@ fun WeatherScreen(viewModel: WeatherViewModel) {
         )
 
         Text(
-            text = "SUNNY",
+            text = stringResource(Res.string.sunny),
             fontSize = 24.sp,
             color = Color.White
         )
---
+++ composeApp/src/commonMain/kotlin/org/khyzhun/sunshine/theme/Colors.kt
@@ -0,0 +1,11 @@
+package org.khyzhun.sunshine.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val PartlyCloudyBackground = Color(0xFFA0C4FF)
+    val RainThunderBackground = Color(0xFF554A99)
+    val RainyBackground = Color(0xFF547FCF)
+    val SnowyBackground = Color(0xFF87D3E8)
+    val SunnyBackground = Color(0xFF4EC0E9)
+}
