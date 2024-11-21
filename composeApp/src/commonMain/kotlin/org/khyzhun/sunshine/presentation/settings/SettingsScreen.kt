52a941ed2c41e2befb695dde0e87e4871f7853f9
khyzhun <sasha.khyzhun@gmail.com>
2024-11-21 23:35:24 +0200

    code polishing

--- composeApp/src/commonMain/kotlin/org/khyzhun/sunshine/presentation/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/khyzhun/sunshine/presentation/settings/SettingsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
@@ -32,13 +33,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.stringResource
 import org.khyzhun.sunshine.core.base.BaseContentLayout
 import org.khyzhun.sunshine.core.base.components.ToolbarPadding
 import org.khyzhun.sunshine.core.theme.AppColors
+import sunshine.composeapp.generated.resources.Res
+import sunshine.composeapp.generated.resources.celsius
+import sunshine.composeapp.generated.resources.dark_theme
+import sunshine.composeapp.generated.resources.enter_location
+import sunshine.composeapp.generated.resources.enter_your_location
+import sunshine.composeapp.generated.resources.save
 
 @Composable
 fun SettingsScreen(
@@ -89,13 +95,13 @@ private fun SettingsScreenContent(
                 TextField(
                     value = cityName,
                     onValueChange = { cityName = it },
-                    label = { Text("City Name") }
+                    label = { Text(stringResource(Res.string.enter_your_location)) }
                 )
                 Button(onClick = {
                     event(SettingsUiEvent.UpdateLocation(cityName))
                     coroutineScope.launch { sheetState.hide() }
                 }) {
-                    Text("Save")
+                    Text(stringResource(Res.string.save))
                 }
             }
         }
@@ -109,39 +115,34 @@ private fun SettingsScreenContent(
         ) {
 
             ToolbarPadding()
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                CloseButton(onClick = onNavigateBack)
-                Text(
-                    text = "Settings screen",
-                    fontSize = 32.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                )
-            }
-
+            CloseButton(onClick = onNavigateBack)
             Spacer(modifier = Modifier.height(48.dp))
 
             LocationField(
                 location = state.location,
                 onClick = {
-                    coroutineScope.launch { sheetState.show() }
+                    coroutineScope.launch {
+                        sheetState.show()
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(0.dp))
+
+            SettingSwitch(
+                label = stringResource(Res.string.dark_theme),
+                isChecked = state.isDarkTheme,
+                onCheckedChange = {
+                    event(SettingsUiEvent.UpdateTheme(it))
                 }
             )
 
             SettingSwitch(
-                label = "Dark Theme",
-                isChecked = state.isDarkTheme,
-                onCheckedChange = { event(SettingsUiEvent.UpdateTheme(it)) }
-            )
-
-            SettingSwitch(
-                label = "Celsius",
+                label = stringResource(Res.string.celsius),
                 isChecked = state.isCelsius,
-                onCheckedChange = { event(SettingsUiEvent.UpdateTemperatureUnit(it)) }
+                onCheckedChange = {
+                    event(SettingsUiEvent.UpdateTemperatureUnit(it))
+                }
             )
         }
     }
@@ -168,16 +169,26 @@ fun LocationField(location: String, onClick: () -> Unit) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = location.ifEmpty { "Enter location" },
+            text = location.ifEmpty {
+                stringResource(Res.string.enter_location)
+            },
             color = Color.White,
             modifier = Modifier.weight(1f)
         )
-        Icon(imageVector = Icons.Default.Edit, tint = Color.White, contentDescription = "Edit Location")
+        Icon(
+            imageVector = Icons.Default.Edit,
+            tint = Color.White,
+            contentDescription = "Edit Location"
+        )
     }
 }
 
 @Composable
-fun SettingSwitch(label: String, isChecked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+fun SettingSwitch(
+    label: String,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
@@ -187,9 +198,33 @@ fun SettingSwitch(label: String, isChecked: Boolean, onCheckedChange: (Boolean) 
             color = Color.White,
             modifier = Modifier.weight(1f)
         )
-        Switch(
+        CustomSwitch(
             checked = isChecked,
-            onCheckedChange = onCheckedChange
+            onCheckedChange = onCheckedChange,
         )
     }
+}
+
+@Composable
+fun CustomSwitch(
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    trackColor: Color = Color(0xE6E6E6), // Light blue for "unchecked" track
+    checkedTrackColor: Color = Color(0xFFFFFF), // Green for "checked" track
+    thumbColor: Color = Color.White // White for thumb
+) {
+    Switch(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = thumbColor,
+            uncheckedThumbColor = thumbColor,
+            checkedTrackColor = checkedTrackColor,
+            uncheckedTrackColor = trackColor,
+            disabledCheckedTrackColor = Color.Gray,
+            disabledUncheckedTrackColor = Color.LightGray,
+        ),
+        modifier = Modifier
+            .size(40.dp) // Adjust size to fit the design
+    )
 }
